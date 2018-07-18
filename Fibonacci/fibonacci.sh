57319baa04eb0dc3c01598fa50091ba4a625934d
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-07-18 14:27:57 +0900

    modified sh

--- Fibonacci/fibonacci.sh
+++ Fibonacci/fibonacci.sh
@@ -6,7 +6,7 @@ number_of_terms=0;
 #javac CrossGene.java Fitness.java Gene.java NQueen.java
 
 # 項数N = 5~35まで5刻みで測定
-for i in `seq 1 7`
+for i in `seq 1 3`
 do 
     for j in `seq 1 10` # 10回測定した平均
     do  
@@ -15,13 +15,13 @@ do
         echo "n="$number_of_terms
 
         echo "go no parallel"
-        (go run fibonacci.go $number_of_terms) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> go_$number_of_terms.txt
+        (go run ./Go/fibonacci.go $number_of_terms) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> ./Go/go_$number_of_terms.txt
 
         echo "go parallel"
-        (go run fibonacci_p.go $number_of_terms) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> go_p_$number_of_terms.txt
+        (go run ./Go/fibonacci_p.go $number_of_terms) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> ./Go/go_p_$number_of_terms.txt
 
         echo "go parallel fix"
-        (go run fibonacci_p_fix.go $number_of_terms) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> go_p_fix_$number_of_terms.txt
+        (go run ./Go/fibonacci_p_fix.go $number_of_terms) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> ./Go/go_p_fix_$number_of_terms.txt
 
         #echo "java"
         #(java NQueen $number_of_terms) | grep "Time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> java_$number_of_terms.txt 
@@ -35,18 +35,18 @@ done
 number_of_terms=0
 
 #平均を求める
-for i in `seq 1 7` #項数N = 5~35(5刻み)
+for i in `seq 1 3` #項数N = 5~35(5刻み)
 do
     number_of_terms=`expr $i \* 5`
    
-    /bin/echo -n $number_of_terms" " >> go_ave.txt
-    cat go_$number_of_terms.txt | awk '{x++;sum+=$1}END {print sum/x}' >> go_ave.txt
+    /bin/echo -n $number_of_terms" " >> ./Go/go_ave.txt
+    cat ./Go/go_$number_of_terms.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Go/go_ave.txt
     
-    /bin/echo -n $number_of_terms" " >> go_p_ave.txt
-    cat go_p_$number_of_terms.txt | awk '{x++;sum+=$1}END {print sum/x}' >> go_p_ave.txt
+    /bin/echo -n $number_of_terms" " >> ./Go/go_p_ave.txt
+    cat ./Go/go_p_$number_of_terms.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Go/go_p_ave.txt
     
-    /bin/echo -n $number_of_terms" " >> go_p_fix_ave.txt
-    cat go_p_fix_$number_of_terms.txt | awk '{x++;sum+=$1}END {print sum/x}' >> go_p_fix_ave.txt
+    /bin/echo -n $number_of_terms" " >> ./Go/go_p_fix_ave.txt
+    cat ./Go/go_p_fix_$number_of_terms.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Go/go_p_fix_ave.txt
     
     #/bin/echo -n $number_of_terms" " >> py2_ave.txt
     #cat py2_$number_of_terms.txt | awk '{x++;sum+=$1}END {print sum/x}' >> py2_ave.txt
