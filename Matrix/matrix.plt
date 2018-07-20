0af9f7b2d633f284ee4aaf3902f543e096667026
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-07-20 14:24:57 +0900

    fix:sh & plt file

--- Matrix/matrix.plt
+++ Matrix/matrix.plt
@@ -3,5 +3,4 @@ set ylabel 'time(sec)'
 set xlabel 'N'
 set title 'Matrix Operation ExecutionSpeed by Golang'
 set output "output_Matrix.svg"
-plot "./Go/go_ave.txt" w lp title "逐次", "./Go/go_thread2_ave.txt" w lp title "並列度2", "./Go/go_thread3_ave.txt" w lp title "並列度3", "./Go/go_thread4_ave.txt" w lp title "並列度4", "./Go/go_thread5_ave.txt" w lp title "並列度5", "./Go/go_thread6_ave.txt" w lp title "並列度6", "./Go/go_thread7_ave.txt" w lp title "並列度7", "./Go/go_thread8_ave.txt" w lp title "並列度8", "./Go/go_thread9_ave.txt" w lp title "並列度9", "./Go/go_thread10_ave.txt" w lp title "並列度10"
- 
+plot "./Go/go_ave.txt" w lp title "逐次", for [i=1:7] sprintf("./Go/go_thread%d_ave.txt", 4**i) w lp title sprintf("並列度%d", 4**i)
--- Matrix/matrix.sh
+++ Matrix/matrix.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env sh
 
+if ls ./**/*.txt > /dev/null 2>&1 ;
+    then rm ./**/*.txt
+fi
+
 size_of_matrix=0;
 
 #コンパイル
 #javac CrossGene.java Fitness.java Gene.java NQueen.java
 
 # N * N(N=100 ~ 1000)の行列の実行時間を測定
-for n in `seq 1 10`
+for n in `seq 1 4`
 do 
-    for j in `seq 1 10` # 10回測定した平均
+    for i in `seq 1 1` # 10回測定した平均
     do  
         size_of_matrix=`expr $n \* 100`
+        thread_size=1
         
         echo "go, N = $size_of_matrix * $size_of_matrix"
         (go run ./Go/matrix.go $size_of_matrix) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> ./Go/go_matrix$size_of_matrix.txt
        
-        for thread_size in `seq 2 10` # 並列度
+        for p in `seq 1 7` # 並列度2**p(p=1~10)まで測定
         do
+            thread_size=`expr $thread_size \* 4`
             echo "go concurrency $thread_size, N = $size_of_matrix * $size_of_matrix"
             
             (go run ./Go/matrix_p.go $size_of_matrix $thread_size) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> ./Go/go_thread${thread_size}_matrix${size_of_matrix}.txt
@@ -34,15 +40,18 @@ done
 size_of_matrix=0
 
 #平均を求める
-for n in `seq 1 10` # N * N(N=100 ~ 1000)の行
+for n in `seq 1 4` # N * N(N=100 ~ 1000)の行
 do
     size_of_matrix=`expr $n \* 100`
+    thread_size=1
     
     /bin/echo -n $size_of_matrix" " >> ./Go/go_ave.txt
     cat ./Go/go_matrix$size_of_matrix.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Go/go_ave.txt
     
-    for thread_size in `seq 2 10` # 並列度
+    for p in `seq 1 7` # 並列度2**p(p=1~10)まで計算
     do
+        thread_size=`expr $thread_size \* 4`
+
         /bin/echo -n $size_of_matrix" " >> ./Go/go_thread${thread_size}_ave.txt
         cat ./Go/go_thread${thread_size}_matrix${size_of_matrix}.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Go/go_thread${thread_size}_ave.txt
         
