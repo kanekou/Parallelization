bd4aca8b7e12d53378a5fb61b06e01ec7b821a15
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-07-21 22:40:26 +0900

    fix:sh files

--- Matrix/matrix_py3.sh
+++ Matrix/matrix_py3.sh
@@ -10,7 +10,7 @@ size_of_matrix=0;
 # N * N(N=100 ~ 1000)の行列の実行時間を測定
 for n in `seq 1 10`
 do 
-    for i in `seq 1 1` # 10回測定した平均
+    for i in `seq 1 10` # 10回測定した平均
     do  
         size_of_matrix=`expr $n \* 100`
         thread_size=1
@@ -18,7 +18,7 @@ do
         echo "Python Sequential, N = $size_of_matrix * $size_of_matrix"
         (python3 ./Python/matrix.py $size_of_matrix) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> ./Python/py3_matrix$size_of_matrix.txt
 
-        for p in `seq 1 3` # 並列度4**p(p=1~8)まで測定
+        for p in `seq 1 3` # 並列度4**p(p=1~3)まで測定
         do
             thread_size=`expr $thread_size \* 4`
 
@@ -44,7 +44,7 @@ do
     /bin/echo -n $size_of_matrix" " >> ./Python/py3_ave.txt
     cat ./Python/py3_matrix$size_of_matrix.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Python/py3_ave.txt
     
-    for p in `seq 1 3` # 並列度4**p(p=1~8)まで計算
+    for p in `seq 1 3` # 並列度4**p(p=1~3)まで計算
     do
         thread_size=`expr $thread_size \* 4`
 
