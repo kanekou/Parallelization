162b3e4fd87aab5ee66f0069a806235431b70bc7
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-07-21 22:35:39 +0900

    fix:separeted sh feils

--- Fibonacci/Python/FibonacciSuccessively.py
+++ Fibonacci/Python/FibonacciSuccessively.py
@@ -7,9 +7,10 @@ def func(n):
     else:
             return func(n-2) + func(n-1)
 
-stratTime = time.time()            
 argv = int(sys.argv[1])
-print('第{0}項 = {1}'.format(argv,func(argv)))
+stratTime = time.time()            
+#print('第{0}項 = {1}'.format(argv,func(argv)))
+func(argv)
 endTime = time.time() #プログラムの終了時刻
 runTime = endTime - stratTime #処理時間
-print('time = {0}s'.format(runTime)) #処理時間を表示
+print('time = {:.10f}sec'.format(runTime)) #処理時間を表示
--- Fibonacci/Python/FibonacciTask.py
+++ Fibonacci/Python/FibonacciTask.py
@@ -8,12 +8,13 @@ def func(n):
     else:
             return func(n-2) + func(n-1)
 
-stratTime = time.time()
 argv = int(sys.argv[1])
+stratTime = time.time()
 if __name__ == "__main__":
         p = Pool(2)
         p.map(func, range(argv))
-print('第{0}項 = {1}'.format(argv,func(argv)))
+#print('第{0}項 = {1}'.format(argv,func(argv)))
+func(argv)
 endTime = time.time() #プログラムの終了時刻
 runTime = endTime - stratTime #処理時間
-print('time = {0}s'.format(runTime)) #処理時間を表示
+print('time = {:.10f}sec'.format(runTime)) #処理時間を表示
