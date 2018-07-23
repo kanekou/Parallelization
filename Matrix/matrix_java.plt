61a3a9760bb7c76e85a1b6a332856dda693cb710
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-07-24 00:23:04 +0900

    mv:NumCPU.go

--- Matrix/matrix_java.plt
+++ Matrix/matrix_java.plt
@@ -4,7 +4,7 @@ set xlabel 'N'
 set title 'Matrix Operation ExecutionSpeed by Java'
 set key left box
 set output "output_Matrix_java.svg"
-plot "./Java/java_ave.txt" w lp title "逐次", for [i=2:8] sprintf("./Java/java_thread%d_ave.txt", i) w lp title sprintf("並列度%d", i)
+plot "./Java/java_ave.txt" w lp title "逐次", for [i=1:4] sprintf("./Java/java_thread%d_ave.txt", i*2) w lp title sprintf("並列度%d", i*2)
 
 
 
--- Matrix/matrix_py3.sh
+++ Matrix/matrix_py3.sh
@@ -10,7 +10,7 @@ size_of_matrix=0;
 # N * N(N=100 ~ 300)の行列の実行時間を測定
 for n in `seq 1 3`
 do 
-    for i in `seq 1 10` # 10回測定した平均
+    for i in `seq 1 1` # 10回測定した平均
     do  
         size_of_matrix=`expr $n \* 100`
         thread_size=1
@@ -21,9 +21,9 @@ do
         echo "Python Sequential numpy, N = $size_of_matrix * $size_of_matrix"
         (python3 ./Python/matrix_np.py $size_of_matrix) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> ./Python/py3_np_matrix$size_of_matrix.txt
 
-        for p in `seq 1 3` # 並列度4**p(p=1~3)まで測定
+        for p in `seq 1 7` # 並列度4**p(p=1~3)まで測定
         do
-            thread_size=`expr $thread_size \* 4`
+            thread_size=`expr $thread_size \+ 2`
 
             echo "Python Concurrency $thread_size, N = $size_of_matrix * $size_of_matrix"
             (python3 ./Python/matrix_p.py $size_of_matrix $thread_size) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> ./Python/py3_thread${thread_size}_matrix${size_of_matrix}.txt
@@ -46,9 +46,9 @@ do
     /bin/echo -n $size_of_matrix" " >> ./Python/py3_np_ave.txt
     cat ./Python/py3_np_matrix$size_of_matrix.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Python/py3_np_ave.txt
 
-    for p in `seq 1 3` # 並列度4**p(p=1~3)まで計算
+    for p in `seq 1 7` # 並列度4**p(p=1~3)まで計算
     do
-        thread_size=`expr $thread_size \* 4`
+        thread_size=`expr $thread_size \+ 1`
 
         /bin/echo -n $size_of_matrix" " >> ./Python/py3_thread${thread_size}_ave.txt
         cat ./Python/py3_thread${thread_size}_matrix${size_of_matrix}.txt  | awk '{x++;sum+=$1}END {print sum/x}' >> ./Python/py3_thread${thread_size}_ave.txt
