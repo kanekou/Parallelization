9d87dbed8949112ce377b86bd515b52e58789e18
Kohei Kaneshima <e165726@ie.u-ryukyu.ac.jp>
2018-07-23 22:44:51 +0900

    fix:java files

--- Matrix/matrix_java.sh
+++ Matrix/matrix_java.sh
@@ -14,7 +14,7 @@ cd ./Java/
 # N * N(N=100 ~ 1000)の行列の実行時間を測定
 for n in `seq 1 10`
 do 
-    for i in `seq 1 10` # 10回測定した平均
+    for i in `seq 1 1` # 10回測定した平均
     do  
         size_of_matrix=`expr $n \* 100`
         thread_size=1
@@ -22,9 +22,9 @@ do
         echo "Java Sequential, N = $size_of_matrix * $size_of_matrix"
         (java MatrixcalSuccessively $size_of_matrix) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> java_matrix$size_of_matrix.txt
        
-        for p in `seq 1 6` # 並列度4**p(p=1~6)まで測定
+        for p in `seq 1 7` # 並列度4**p(p=1~3)まで測定
         do
-            thread_size=`expr $thread_size \* 4`
+            thread_size=`expr $thread_size \+ 1`
             
             echo "Java Concurrency $thread_size, N = $size_of_matrix * $size_of_matrix"
             (java MatrixcalTask $size_of_matrix $thread_size) | grep "time" | egrep -o '[0-9]+[\.]+[0-9]+[0-9]'>> java_thread${thread_size}_matrix${size_of_matrix}.txt
@@ -47,9 +47,9 @@ do
     /bin/echo -n $size_of_matrix" " >> ./Java/java_ave.txt
     cat ./Java/java_matrix$size_of_matrix.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Java/java_ave.txt
     
-    for p in `seq 1 6` # 並列度4**p(p=1~6)まで計算
+    for p in `seq 1 7` # 並列度4**p(p=1~3)まで計算
     do
-        thread_size=`expr $thread_size \* 4`
+        thread_size=`expr $thread_size \+ 1`
         
         /bin/echo -n $size_of_matrix" " >> ./Java/java_thread${thread_size}_ave.txt
         cat ./Java/java_thread${thread_size}_matrix${size_of_matrix}.txt | awk '{x++;sum+=$1}END {print sum/x}' >> ./Java/java_thread${thread_size}_ave.txt
